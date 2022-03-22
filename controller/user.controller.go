fe047739b49724313d4090c7a822d7792825b226
hank <akysn564@gmail.com>
2022-03-22 09:24:51 +0800

    first init

---
+++ controller/user.controller.go
@@ -0,0 +1,53 @@
+package controllsers
+
+import (
+	"net/http"
+	"playground/models"
+	"playground/services"
+
+	"github.com/gin-gonic/gin"
+)
+
+type UserController struct {
+	UserService services.UserService
+}
+
+func New(userservice services.UserService) UserController {
+	return UserController{
+		UserService: userservice,
+	}
+}
+
+func (uc *UserController) CreateUser(ctx *gin.Context) {
+	var user models.User
+	if err := ctx.ShouldBindJSON(&user); err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{"message": err.Error()})
+		return
+	}
+	ctx.JSON(200, "")
+}
+
+func (uc *UserController) GetUser(ctx *gin.Context) {
+	ctx.JSON(200, "")
+}
+
+func (uc *UserController) GetAll(ctx *gin.Context) {
+	ctx.JSON(200, "")
+}
+
+func (uc *UserController) UpdateUser(ctx *gin.Context) {
+	ctx.JSON(200, "")
+}
+
+func (uc *UserController) DeleteUser(ctx *gin.Context) {
+	ctx.JSON(200, "")
+}
+
+func (uc *UserController) RegisterUserRoutes(rg *gin.RouterGroup) {
+	userroute := rg.Group("/user")
+	userroute.POST("/create", uc.CreateUser)
+	userroute.GET("/get/:name", uc.GetUser)
+	userroute.GET("/getall", uc.GetAll)
+	userroute.PATCH("/update", uc.UpdateUser)
+	userroute.DELETE("/delete", uc.DeleteUser)
+}
---
+++ go.mod
@@ -0,0 +1,9 @@
+module playground
+
+go 1.16
+
+require (
+	github.com/gin-gonic/gin v1.7.7 // indirect
+	github.com/rs/cors/wrapper/gin v0.0.0-20220223021805-a4a5ce87d5a2
+	go.mongodb.org/mongo-driver v1.8.4 // indirect
+)
