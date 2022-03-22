0695588e695b530d9f668ae8d7595a9d3830737d
hank <akysn564@gmail.com>
2022-03-22 13:31:09 +0800

    GetUser

--- controller/user.controller.go
+++ controller/user.controller.go
@@ -24,10 +24,17 @@ func (uc *UserController) CreateUser(ctx *gin.Context) {
 		ctx.JSON(http.StatusBadRequest, gin.H{"message": err.Error()})
 		return
 	}
-	ctx.JSON(200, "")
+	err := uc.UserService.CreateUser(&user)
+	if err != nil {
+		ctx.JSON(http.StatusBadGateway, gin.H{"message": err.Error()})
+		return
+	}
+	ctx.JSON(http.StatusOK, gin.H{"message": "success"})
 }
 
 func (uc *UserController) GetUser(ctx *gin.Context) {
+	username := ctx.Param("name")
+	uc.UserService.GetUser(&username)
 	ctx.JSON(200, "")
 }
 
